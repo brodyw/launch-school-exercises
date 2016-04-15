3835db85e1176b4ad2c4b63ba21a332107d067e0
Brody <brody.west25@gmail.com>
2016-04-15 06:23:27 +0000

    Finished all the hash exercises

---
+++ immediate_family.rb
@@ -0,0 +1,5 @@
+# immediate_family.rb
+
+family = { uncles: ["bob", "joe", "steve"], sisters: ["jane", "jill", "beth"], brothers: ["frank","rob","david"], aunts: ["mary","sally","susan"] }
+
+p family.select{ |k, v| k == :sisters || k == :brothers }.values.to_a.flatten
